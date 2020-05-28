3bfe529ed689102bb24e572c38868e795c11198c
Jasmine Silfver <jasmine.silfver@gmail.com>
2020-05-28 15:45:42 +0200

    I can now add a photo

--- controllers/photo_controller.js
+++ controllers/photo_controller.js
@@ -2,7 +2,9 @@
  * Photo controller
  */
 
-const {User} = require('../models');
+const {User, Photo} = require('../models');
+
+const {matchedData, validationResult} = require('express-validator')
 
 
 /**
@@ -54,11 +56,37 @@ const show = async (req, res) => {
 /**
  * Add a new photo
  */
-const store = (req, res) => {
-    res.send({
-        status: 'success',
-        data: 'This is where I will be able to add a new photo'
-    })
+const store = async (req, res) => {
+	const errors = validationResult(req)
+	if(!errors.isEmpty()) {
+        res.status(422).send({
+            status: 'fail',
+            data: errors.array()
+		})
+		return
+	}
+	
+	const validData = matchedData(req)
+	validData.user_id = req.user.id
+
+	
+	try {
+		const photo = await Photo.forge(validData).save()
+
+		res.send({
+			status: 'success',
+			data: {
+				photo
+			}
+		})
+	} catch(error) {
+		res.status(500).send({
+			status: 'error',
+			message: 'Exception thrown in database when trying to add a new photo'
+		})
+		throw error
+
+	}
 }
 
 module.exports = {
---
+++ validation/photo_validation.js
@@ -0,0 +1,11 @@
+const {body} = require('express-validator')
+
+const newPhotoRules = [
+    body('title').trim().isLength({min: 3}),
+    body('url').trim().isURL(),
+    body('comment')
+]
+
+module.exports = {
+    newPhotoRules
+}
